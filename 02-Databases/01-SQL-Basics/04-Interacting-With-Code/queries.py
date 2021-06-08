6ad4f42974bf0007d8d0d049cc819ba064d64092
Edward Schults <edward.schults@gmail.com>
2021-06-08 18:10:41 +0200

    import challenges from data-challenges and python-beginners

---
+++ 02-Databases/01-SQL-Basics/04-Interacting-With-Code/queries.py
@@ -0,0 +1,24 @@
+# pylint: disable=missing-docstring, C0103
+
+def directors_count(db):
+    # Return the number of directors contained in the database
+    pass
+
+def directors_list(db):
+    # Return the list of all the directors sorted in alphabetical order
+    pass
+
+
+def love_movies(db):
+    # return the list of all movies which contain the word "love" in their title, sorted in alphabetical order
+    pass
+
+
+def directors_named_like_count(db, name):
+    # return the number of directors which contain a given word in their name
+    pass
+
+
+def movies_longer_than(db, min_length):
+    # return this list of all movies which are longer than a given duration, sorted in the alphabetical order
+    pass
